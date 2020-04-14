6d155bd4d62e5caa0e4be2f08cd1c416db28b672
educhinski <educhinski650@gmail.com>
2020-04-14 18:44:47 +0300

    routing- added some routes

--- app.py
+++ app.py
@@ -4,9 +4,22 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "Server works!"
+    return "Index Page"
 
 
-@app.route('/greet')
+@app.route('/hello')
 def say_hello():
-    return "Hello from Server"
+    return "Hello, greetings from different endpoint"
+
+
+# adding variables
+@app.route('/user/<username>')
+def show_user(username):
+    # returns the username
+    return 'Username: %s' % username
+
+
+@app.route('/post/<int:post_id>')
+def show_post(post_id):
+    # returns the post, the post_id should be an int
+    return str(post_id)
