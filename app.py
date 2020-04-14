ce7325f7d2a3103c177d36298b1d1de35d95cfac
educhinski <educhinski650@gmail.com>
2020-04-14 21:17:04 +0300

    added a demo request route

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
 
@@ -13,7 +13,7 @@ def say_hello():
 
 
 # adding variables
-@app.route('/user/<username>')
+@app.route('/username/<username>')
 def show_user(username):
     # returns the username
     return 'Username: %s' % username
@@ -23,3 +23,14 @@ def show_user(username):
 def show_post(post_id):
     # returns the post, the post_id should be an int
     return str(post_id)
+
+
+# shows a demo request route
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        # check user details from db
+        login_user()
+    elif request.method == 'GET':
+        # serve login page
+        serve_login_page()
